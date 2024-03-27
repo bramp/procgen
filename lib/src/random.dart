d890882f24e82655ecc49d619ccfcc59da0f0402
Andrew Brampton <github@bramp.net>
2024-03-27 11:13:51 -0700

    Add random tags, refactored the widgets a little, fixed a bug if water had two clip regions.

--- lib/src/random.dart
+++ lib/src/random.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:collection/collection.dart';
+
 extension RandomExt on Random {
   /// Returns a random double that is normally distributed around [min, max]
   /// centered at (max - min) / 2.
@@ -13,5 +15,33 @@ extension RandomExt on Random {
       sum += nextDouble();
     }
     return (sum / iterations) * (max - min) + min;
+  }
+}
+
+extension ListRandomExt<T> on List<T> {
+  /// Returns a random element from this list.
+  T random(Random rng) => this[rng.nextInt(length)];
+}
+
+extension IterableRandomExt<T> on Iterable<T> {
+  /// Returns a random element from this list.
+  T random(Random rng) => elementAt(rng.nextInt(length));
+
+  /// Return a random element based on the [weights] provided.
+  T weighted(Random rng, List<double> weights) {
+    if (length != weights.length) {
+      throw ArgumentError('Length of weights must match length of container');
+    }
+
+    final z = rng.nextDouble() * weights.sum;
+    var acc = 0.0;
+    for (int i = 0; i < weights.length; i++) {
+      acc += weights[i];
+      if (z <= acc) {
+        return elementAt(i);
+      }
+    }
+
+    return last;
   }
 }
